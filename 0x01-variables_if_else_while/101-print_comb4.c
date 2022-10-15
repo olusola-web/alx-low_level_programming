0ae79b5569acee0a63092eb7356eb1aef5922ea8
olusola-web <olusolakorode@gmail.com>
2022-10-14 21:04:40 -0700

    another 1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,20 +6,24 @@
  */
 int main(void)
 {
-	int n, m, l;
+	int m, i, j;
 
-	for (n = 48; n < 58; n++)
+	for (i = '0'; i < '9'; i++)
 	{
-		for (m = 49; m < 58; m++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			for (l = 50; l < 58; l++)
+			for (m = '0'; m <= '9'; m++)
 			{
-				if (l > m && m > n)
+				if (i > j && j > m)
 				{
-					putchar(n);
+					putchar(i);
+					putchar(j);
 					putchar(m);
-					putchar(1);
-					if (n != 55 || m != 56)
+					if (i  == '7' && j == '8' && m == '9')
+					{
+						break;
+					}
+					else
 					{
 						putchar(',');
 						putchar(' ');
