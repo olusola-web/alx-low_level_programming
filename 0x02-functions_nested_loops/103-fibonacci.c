2b99c5f83d83a2e28a5d8a09dccb62b28bf0df45
olusola-web <olusolakorode@gmail.com>
2022-10-18 14:50:20 -0700

    new message

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: Keenmuhammad
+ */
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the fibinacci
+ * Return: 0 always
+ */
+int main(void)
+{
+long int i, x = 1, y = 2, sum = 0, tSum = 0;
+for (i = 0; i < 49; i++)
+{
+if ((y % 2 == 0) && (y <= 4000000))
+{
+tSum = tSum + y;
+}
+sum = x + y;
+x = y;
+y = sum;
+}
+printf("%ld\n", tSum);
+return (0);
+}
