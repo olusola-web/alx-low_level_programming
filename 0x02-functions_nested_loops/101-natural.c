723ba0f07c06fd12c78b651ea4cf41c84cc24239
olusola-web <olusolakorode@gmail.com>
2022-10-18 14:35:01 -0700

    new message

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: Keenmuhammad
+ */
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check for multiples of 3 and 5
+ * Return: 0 always
+ */
+int main(void)
+
+{
+	int x = 1024, y, sum = 0;
+
+	for (y = 0; y < x; y++)
+
+	{
+		if ((y % 3 == 0) || (y % 5 == 0))
+
+		{
+			sum = sum + y;
+
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+
+}
