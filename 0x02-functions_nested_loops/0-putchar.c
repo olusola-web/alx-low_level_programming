e0610db5b0a7f92c256b8e6cd0e3add505e4c57a
olusola-web <olusolakorode@gmail.com>
2022-10-18 09:16:57 -0700

    new message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
-/**
- * main - program that prints Holberton.
- *
- * Return: int
- */
 
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i = 0;
-	char strng_ptchr[8] = "_putchar";
-
-	while (i < 8)
-	{
-		_putchar(strng_ptchr[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
